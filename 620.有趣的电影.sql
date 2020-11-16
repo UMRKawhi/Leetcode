c29aae88f8ef9ff85b73e57fc1131373bbbeff5e
UMRKawhi <48422665+UMRKawhi@users.noreply.github.com>
2020-11-17 07:13:43 +1100

    Create 620.有趣的电影.sql

---
+++ 620.有趣的电影.sql
@@ -0,0 +1,15 @@
+--
+-- @lc app=leetcode.cn id=620 lang=mysql
+--
+-- [620] 有趣的电影
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT id, movie, description, rating
+FROM cinema
+WHERE description <> 'boring'
+AND id % 2 = 1
+ORDER BY rating DESC;
+-- @lc code=end
+
